f6526d9d4415ff34914abdbe072911d23bd6c3ee
KhoyaGasa <mgskenneth50@gmail.com>
2021-06-01 11:29:45 +0300

    newest changes

--- uace.py
+++ uace.py
@@ -1,25 +1,29 @@
-#A program that calculates the UACE points scored by someone
-#Create a dictionary containing the scores and their respective points.
-points_source = {'A': 6, 'B': 5, 'C': 4, 'D': 3, 'E': 2, 'O': 1, 'F': 0} 
+#A program that calculates the UACE points scored by someone 
 #Print and read user input
-print("Please enter the scores in first principle subject")
-first_subject = input()
-print ("Please enter the score for the second principle subject")
-second_subject = input()
-print ("Please enter the score for the third principle subject")
-third_subject = input()
-#Search for the corresponding points in the dictionary
-num_points = points_source[first_subject]
-num_points2 = points_source[second_subject]
-num_points3 = points_source[third_subject]
-#Subsidiary Check
-print("Did you score the subsidiary passes Y/N?")
-sub_test = input()
-pass_onot = ["Y", "N"]
-if sub_test == pass_onot[0]:   
-    #Add up the points then display them
-    total_points = num_points+num_points2+num_points3+2
-    print("Your total points scored are" + str(total_points))
-else:
-    total_points = num_points+num_points2+num_points3
-    print("Your total points scored are" + str(total_points))
+print("Please enter your scores in the format 1AAA1")
+scores = input()
+values = list(scores)
+def machine (values):
+    #Create a dictionary containing the scores and their respective points.
+    points_source = {'a': 6, 'b': 5, 'c': 4, 'd': 3, 'e': 2, 'o': 1, 'f': 0}
+    #Search for the corresponding points in the dictionary
+    num_points = points_source[values[1]]
+    num_points2 = points_source[values[2]]
+    num_points3 = points_source[values[3]]
+    a = int(values[0])
+    b = int(values[4])
+    limit =int(8)
+    if a<=limit and a>0 and b<=limit and b>0 :   
+        #Add up the points then display them
+        total_points = num_points+num_points2+num_points3+2
+        print("Your total points scored are" + str(total_points))
+    elif a>0 and a>limit and b<=limit and b>0 :
+        total_points = num_points+num_points2+num_points3+1
+        print("Your total points scored are" + str(total_points))
+    elif a<=limit and a>0 and b>0 and b>limit:
+        total_points = num_points+num_points2+num_points3+1
+        print("Your total points scored are" + str(total_points))
+    else:
+        total_points = num_points+num_points2+num_points3
+        print("Your total points scored are" + str(total_points))
+machine(values)
