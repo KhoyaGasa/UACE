2ded893e768954bfa89dc1573de7c32c7fe70e8f
KhoyaGasa <mgskenneth50@gmail.com>
2021-06-02 12:20:01 +0300

    New variable and function names

--- uace.py
+++ uace.py
@@ -1,29 +1,29 @@
 #A program that calculates the UACE points scored by someone 
 #Print and read user input
 print("Please enter your scores in the format 1AAA1")
-scores = input()
-values = list(scores)
-def machine (values):
-    #Create a dictionary containing the scores and their respective points.
-    points_source = {'a': 6, 'b': 5, 'c': 4, 'd': 3, 'e': 2, 'o': 1, 'f': 0}
+UserInput = input()
+IndividualScores = list(UserInput)
+def computeUACEpoints (IndividualScores):
+    #Create subsidiaryScore1 dictionary containing the scores and their respective points.
+    gradingScale = {'a': 6, 'b': 5, 'c': 4, 'd': 3, 'e': 2, 'o': 1, 'f': 0}
     #Search for the corresponding points in the dictionary
-    num_points = points_source[values[1]]
-    num_points2 = points_source[values[2]]
-    num_points3 = points_source[values[3]]
-    a = int(values[0])
-    b = int(values[4])
+    principalScore1 = gradingScale[IndividualScores[1]]
+    principalScore2 = gradingScale[IndividualScores[2]]
+    principalScore3 = gradingScale[IndividualScores[3]]
+    subsidiaryScore1 = int(IndividualScores[0])
+    subsidiaryScore2 = int(IndividualScores[4])
     limit =int(8)
-    if a<=limit and a>0 and b<=limit and b>0 :   
+    if subsidiaryScore1<=limit and subsidiaryScore1>0 and subsidiaryScore2<=limit and subsidiaryScore2>0 :   
         #Add up the points then display them
-        total_points = num_points+num_points2+num_points3+2
+        total_points = principalScore1+principalScore2+principalScore3+2
         print("Your total points scored are" + str(total_points))
-    elif a>0 and a>limit and b<=limit and b>0 :
-        total_points = num_points+num_points2+num_points3+1
+    elif subsidiaryScore1>0 and subsidiaryScore1>limit and subsidiaryScore2<=limit and subsidiaryScore2>0 :
+        total_points = principalScore1+principalScore2+principalScore3+1
         print("Your total points scored are" + str(total_points))
-    elif a<=limit and a>0 and b>0 and b>limit:
-        total_points = num_points+num_points2+num_points3+1
+    elif subsidiaryScore1<=limit and subsidiaryScore1>0 and subsidiaryScore2>0 and subsidiaryScore2>limit:
+        total_points = principalScore1+principalScore2+principalScore3+1
         print("Your total points scored are" + str(total_points))
     else:
-        total_points = num_points+num_points2+num_points3
+        total_points = principalScore1+principalScore2+principalScore3
         print("Your total points scored are" + str(total_points))
-machine(values)
+computeUACEpoints(IndividualScores)
